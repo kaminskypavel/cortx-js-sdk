9417ad5bbb5f5f3f585d715f2fb38426c4cd452f
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2021-04-27 17:09:38 +0300

    feat: add initila implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('hello');
+// Create Client
+import AWS from 'aws-sdk';
+import https from 'https';
+
+
+class CORTX_S3 extends AWS.S3 {
+  // @ts-ignore
+  private endpoint: string;
+  private accessKeyId: string;
+  private secretAccessKey: string;
+
+
+  constructor(endpoint: string, accessKeyId: string, secretAccessKey: string) {
+    super({
+      accessKeyId,
+      secretAccessKey,
+      endpoint,
+      s3ForcePathStyle: true,
+      httpOptions: {
+        agent: new https.Agent({rejectUnauthorized: false})
+      }
+    });
+
+    this.endpoint = endpoint;
+    this.accessKeyId = accessKeyId;
+    this.secretAccessKey = secretAccessKey;
+  }
+}
+
+
+export default {
+  CORTX_S3
+};
