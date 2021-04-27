659465d5762846019d557f2ee8fb8c909330922d
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2021-04-27 17:18:26 +0300

    feat(examples): integrate dotenv so user can easily run eample from environment parameters and connect their own CORTX instance

--- example/src/listObjects.ts
+++ example/src/listObjects.ts
@@ -3,5 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 (async () => {
+
+  const {S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY, S3_ENDPOINT} = process.env;
+  console.log(S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY, S3_ENDPOINT);
   console.log('example');
 })();
